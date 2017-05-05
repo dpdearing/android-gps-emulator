019ffca939b88d6d2778c029d2ee2c8ff71ead51
Dave Dearing <dpdearing@gmail.com>
2017-05-04 20:23:56 -0700

    Fix for issue #15 - use the emulator authentication token after first connecting via telnet

--- src/main/java/com/dpdearing/sandbox/gpsemulator/server/LocationServiceImpl.java
+++ src/main/java/com/dpdearing/sandbox/gpsemulator/server/LocationServiceImpl.java
@@ -1,10 +1,14 @@
 /**
- * Copyright (c) 2010 David Dearing
+ * Copyright (c) 2010-17 David Dearing
  */
 
 package com.dpdearing.sandbox.gpsemulator.server;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.dpdearing.sandbox.gpsemulator.common.LocationService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -15,18 +19,40 @@ import de.mud.telnet.TelnetWrapper;
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class LocationServiceImpl extends RemoteServiceServlet implements
-      LocationService {
+public class LocationServiceImpl extends RemoteServiceServlet implements LocationService {
+
+   private static Logger logger =
+           Logger.getLogger(LocationServiceImpl.class.getName());
+
    /**
     * The telnet wrapper.
     */
-   private TelnetWrapper _telnet;
+   final private TelnetWrapper _telnet;
+
+   /**
+    * The emulator authentication token
+    */
+   private String _emulator_console_auth_token = null;
 
    /**
     * Constructor.
     */
    public LocationServiceImpl() {
       _telnet = new TelnetWrapper();
+
+      //TODO does this work on all platforms?
+      final String home = System.getProperty("user.home");
+      final File authTokenFile = new File(home, ".emulator_console_auth_token");
+      try {
+         // read the entire contents of the
+         _emulator_console_auth_token = new Scanner(authTokenFile).useDelimiter("\\Z").next();
+         logger.info("Loaded emulator_console_auth_token " + _emulator_console_auth_token);
+      } catch (IOException ioe) {
+         logger.log(Level.SEVERE,
+                 "Failed to read the emulator_console_auth_token from " +
+                         authTokenFile.getAbsolutePath(),
+                 ioe);
+      }
    }
    
    /**
@@ -35,8 +61,15 @@ public class LocationServiceImpl extends RemoteServiceServlet implements
    public void setPort(final int port) throws IOException {
       // disconnect from any previous connection
       _telnet.disconnect();
-      // reconnect to the new port
+      // connect to the specified port
+      logger.info("Connecting to emulator at localhost:" + port);
       _telnet.connect("localhost", port);
+      if (_emulator_console_auth_token == null) {
+         logger.info("No emulator_console_auth_token; skipping authentication");
+      } else {
+         logger.info("Authenticating with emulator_console_auth_token: " + _emulator_console_auth_token);
+         _telnet.send("auth " + _emulator_console_auth_token);
+      }
    }
    
    /**
